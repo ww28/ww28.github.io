1b162ce591653926fe989e250b5f6ee18712c109
kid rendom pulsar <75491595+MichnoAZ@users.noreply.github.com>
2022-12-10 21:30:11 +0500

    Update ambient.js

--- src/Sounds/ambient.js
+++ src/Sounds/ambient.js
@@ -1,4 +1,4 @@
-let audio = new Audio("https://github.com/ww28/ww28.github.io/blob/main/src/Sounds/Y2Mate.is%20-%20Marvelous%20Jungle%20Sounds-XdZkJhD8NhE-160k-1654375471967.mp3");
+let audio = new Audio("https://github.com/ww28/ww28.github.io/blob/main/src/Sounds/Y2Mate.is%20-%20Marvelous%20Jungle%20Sounds-XdZkJhD8NhE-160k-1654375471967.mp3?raw=true");
 
 audio.addEventListener('canplaythrough', function() {
     audio.loop = 1;
