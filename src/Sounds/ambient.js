6891757b7e800b8b497b251223b25289a189d4a8
kid rendom pulsar <75491595+MichnoAZ@users.noreply.github.com>
2022-12-10 22:09:13 +0500

    Update ambient.js

--- src/Sounds/ambient.js
+++ src/Sounds/ambient.js
@@ -4,3 +4,9 @@ audio.loop = 1;
 audio.addEventListener("canplaythrough", e => {
     audio.play();
 });
+
+let hit = new Audio("https://github.com/ww28/ww28.github.io/blob/main/src/Sounds/Y2Mate.is%20-%20Metal%20hit%20Sound%20Effect-wOh41654QFg-128k-1654375375140.mp3?raw=true");
+
+window.addEventListener('click', function() {
+    hit.play();
+});
