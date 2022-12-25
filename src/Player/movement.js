bb561abed4b5e0a017865f1bf1c4940bf01d1a31
kid rendom pulsar <75491595+MichnoAZ@users.noreply.github.com>
2022-12-25 21:31:25 +0500

    Update movement.js
    Added linear interpolation for movement

--- src/Player/movement.js
+++ src/Player/movement.js
@@ -1,18 +1,20 @@
 const playerSpeed = 11;
+function lerp(oldpos, pos) {
+    return oldpos + (pos - oldpos) * 0.5;
+}
 
 window.addEventListener("keydown", e => {
     switch(e.keyCode) {
         case 87:
-             window.playerPos.y-= playerSpeed
+             window.playerPos.y = lerp(window.playerPos.y - playerSpeed)
         break;
         case 83:
-             window.playerPos.y+= playerSpeed
+             window.playerPos.y = lerp(window.playerPos.y, window.playerPos.y + playerSpeed)
         break;
         case 68:
-             window.playerPos.x -= playerSpeed
-        break;
+             window.playerPos.x = lerp(window.playerPos.x, window.playPos.x - playerSpeed);
         case 65:
-             window.playerPos.x += playerSpeed
+             window.playerPos.x = lerp(window.playerPos.x, window.playerPos.x + playerSpeed)
         break;
     }
 })
